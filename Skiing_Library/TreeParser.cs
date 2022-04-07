7be0d9d780d8fed90d9ee274df1b554c28bcfa1c
Caleb Wightman <59291368+Cwighty@users.noreply.github.com>
2022-04-06 18:08:24 -0600

    Read file parsing test passing

--- Skiing_Library/TreeParser.cs
+++ Skiing_Library/TreeParser.cs
@@ -2,14 +2,33 @@
 {
     public static class TreeParser
     {
-        public static string[] ReadFile(string v)
+        public static string[] ReadFile(string filePath)
         {
-            throw new NotImplementedException();
+            return System.IO.File.ReadAllLines(filePath);
         }
 
-        public static bool[,] Parse(string[] vs)
+        public static bool[,] Parse(string[] lines)
         {
-            throw new NotImplementedException();
+            var treeBitMap = new bool[lines.Length, lines[0].Length];
+            int row = 0;
+            foreach (var line in lines)
+            {
+                int col = 0;
+                foreach(char character in line)
+                {
+                    if (character == '#')
+                    {
+                        treeBitMap[row,col] = true;
+                    }
+                    else
+                    {
+                        treeBitMap[row,col] = false;
+                    }
+                    col++;
+                }
+                row++;
+            }
+            return treeBitMap;
         }
     }
 }
